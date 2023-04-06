ae6cebbed15bd9cdbfdde390f4d4581f3bf94861
Isaac M Bazian <bazia001@umn.edu>
2023-04-06 10:04:59 -0500

    Answered questions, will double check parallelization results later

--- R/week11-local.R
+++ R/week11-local.R
@@ -167,13 +167,37 @@ table1_tbl <- tibble(
 ) 
 
 
-
-Table2_tbl <- tibble(
+# This code makes a tibble of runtime for each model run sequentially and parallel
+table2_tbl <- tibble(
   algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"),
   original = c(tocOLS$callback_msg, tocElasticNet$callback_msg, tocRandomForest$callback_msg, tocXGB$callback_msg),
   parallelized = c(tocOLSPar$callback_msg, tocElasticNetPar$callback_msg, tocRandomForestPar$callback_msg, tocXGBPar$callback_msg)
 )
 
+#Q1: It seems that eXtreme Gradient Boosting benefited the most from parallelization,
+#shaving roughly 25 seconds off the runtime. I imagine that this is because it
+#has a relatively long runtime relative to OLS and Elastic Net, so there's actually
+#space to notice considerable improvement. The Random Forest seemed to not improve
+#much, despite also being a relatively long run time - based on some quick Googling,
+#the 'ranger' method may already be using some kind of processing tricks that
+#make parallelization less relatively impactful, but I will need to double check this.
+
+#Q2: The fastest parallelized model was the Elastic Net at roughly 1 second, and
+#the slowest was the XGB at roughly 87 seconds, for a differences of roughly 86 seconds.
+#However, it is worth noting that the OLS model bears the brunt of the startup time
+#when initiating parallelization. XGB was already slower than EN before parallelization,
+#by roughly 110 seconds, so the gap certainly narrowed, but XGB is still a more
+#complex algorithm than EN due to more hyperparameters, so it's only natural that
+#it would still take longer, even if they both benefit from parallelization.
+
+#Q3: I would still recommend the Random Forest model in this case - it still
+#produces the highest ho_rsq, which is the prediction metric we care about,
+#and even with parallelization it is faster than XGB. Having said that, it would
+#be worth comparing prediction and speed if different data is being used or more
+#processing power is available - with enough cores and large enough datasets, 
+#the XGB model may run much faster than the Random Forest, which would potentially
+#be more valuable than marginal gains in predictive power (and if XGB is both faster
+#and better at predicting, it becomes no contest)
 
 
 
